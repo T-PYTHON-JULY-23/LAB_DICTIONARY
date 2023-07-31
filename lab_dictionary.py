a518f563e0962fb88d76479c100f900c29258fc7
SarahAlhamad <140398026+SarahAlhamad@users.noreply.github.com>
2023-07-31 09:50:05 +0300

    Update lab_dictionary.py

--- lab_dictionary.py
+++ lab_dictionary.py
@@ -1,16 +1,16 @@
 phone_number= {
-    "Amal":"05683232222",
-    "Mohammed":"0522222232",
-    "Khadijah":"0532335983",
-    "Abdullah":"0545341144" ,
-    "Rawan ":"0545534556" ,
-    "Faisal":"0560664566",
-    "Layla":"0567917077" 
+    "05683232222":"Amal",
+    "0522222232":"Mohammed",
+   "0532335983":"Khadijah",
+    "0545341144":"Abdullah" ,
+    "0545534556":"Rawan" ,
+    "0560664566":"Faisal",
+    "0567917077" :"Layla"
     }
 
 number = input (" please enter phone number: ")
 
-if len(number) < 10 or number.isdigit() != True:
+if len(number) < 10 or not number.isdigit() :
      print ("this is invalid number")
 else:
     user = phone_number.get(number,"Sorry, the number not found ")
@@ -21,13 +21,18 @@ else:
 
 
 #Q2
+
 def new_list():
-    num_list = [5,0,34,9,0,13,8]
+    num_list=[5,0,34,9,0,13,8]
     for i in num_list:
         if i == 0:
-            list.remove(0)
-            list.append(0) 
-        return num_list
+            num_list.remove(0)
+            num_list.append(0)
+    print(num_list)
+        
+new_list()
+
+
 
 print(new_list())
 
